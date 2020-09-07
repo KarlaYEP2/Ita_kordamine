5503f7b17e04cac266c18b6ae07fc28ee69629fc
KarlaYEP2 <karlerik2013@gmail.com>
2020-09-07 14:32:58 +0300

    laulu klass

---
+++ Menüü.py
@@ -0,0 +1,7 @@
+from album import album
+from laul import laul
+
+laul1 = laul("test", "test2")
+print(laul1.laulja, laul1.pealkiri)
+laul2 = laul("test", "test22")
+print(laul2.laulja, laul2.pealkiri)
