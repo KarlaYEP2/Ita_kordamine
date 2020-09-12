b70ef9f53a14e4b6ad553d67f1d0cbda6c201998
KarlaYEP2 <70624455+KarlaYEP2@users.noreply.github.com>
2020-09-12 21:18:29 +0300

    Laulja

--- laulja.py
+++ laulja.py
@@ -3,6 +3,15 @@ class Laulja:
     def __init__(self, nimi):
         self.nimi = nimi
         self.albumid = []
-
+        self.laulud = []
     def lisa_album(self, album):
         self.albumid.append(album)
+        for i in album.albumid:
+            self.laulud.append(i)
+    def nimetused(self):
+        print(self.nimi)
+
+    def albumiid(self):
+        for album in self.albumid:
+            album.nimetused()
+
