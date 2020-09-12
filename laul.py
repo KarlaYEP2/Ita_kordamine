3904b90eddeda1a08dffcc1df1c1b1def90d61c4
KarlaYEP2 <70624455+KarlaYEP2@users.noreply.github.com>
2020-09-12 21:18:05 +0300

    Klass Laul

--- laul.py
+++ laul.py
@@ -1,5 +1,9 @@
 class Laul:
 
-    def __init__(self,pealkiri,laulja):
+    def __init__(self,pealkiri,laulja, album):
         self.pealkiri = pealkiri
         self.laulja = laulja
+        self.album = album
+
+    def toopealkirjad(self):
+        print("  " + self.pealkiri + "  ")
